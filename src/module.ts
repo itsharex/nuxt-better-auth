a79f0967dd68aecdbc54df01977583796363a187
onmax <maximogarciamtnez@gmail.com>
2025-12-18 17:02:44 +0100

    fix: read build-time env var for runtimeConfig

--- src/module.ts
+++ src/module.ts
@@ -50,7 +50,7 @@ export default defineNuxtModule<BetterAuthModuleOptions>({
       useDatabase: hasHubDb,
     }) as { redirects: { login: string, guest: string }, useDatabase: boolean }
 
-    nuxt.options.runtimeConfig.betterAuthSecret ??= ''
+    nuxt.options.runtimeConfig.betterAuthSecret ||= process.env.NUXT_BETTER_AUTH_SECRET || ''
     nuxt.options.runtimeConfig.auth = defu(nuxt.options.runtimeConfig.auth as Record<string, unknown>, {
       secondaryStorage: secondaryStorageEnabled,
     }) as { secondaryStorage: boolean }
